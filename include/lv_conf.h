a022c434288230f0a6f4263516b53fb316de7401
Tengfei Jiang <jiangtfcz99@gmail.com>
2026-06-28 22:54:21 +0800

    Add LittleFS partition + LVGL PNG decoder config (准备工作)
    - partitions.csv: copy of default_8MB (1.5MB spiffs/littlefs partition)
    - platformio.ini: board_build.partitions + filesystem=littlefs
    - lv_conf.h: enable LV_USE_PNG=1, LV_USE_FS_POSIX=1,
      drive letter 'L' mounted at /littlefs, 4KB read cache
    - data/: 16 PNG files copied from src/icon/ with simple names
      (volume variants reuse volume.png as placeholder for now; track.png reuses video.png)
    - Adding new icons: drop PNG into data/, run 'pio run -t uploadfs'

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -617,11 +617,11 @@
 #endif
 
 /*API for open, read, etc*/
-#define LV_USE_FS_POSIX 0
+#define LV_USE_FS_POSIX 1
 #if LV_USE_FS_POSIX
-    #define LV_FS_POSIX_LETTER '\0'     /*Set an upper cased letter on which the drive will accessible (e.g. 'A')*/
-    #define LV_FS_POSIX_PATH ""         /*Set the working directory. File/directory paths will be appended to it.*/
-    #define LV_FS_POSIX_CACHE_SIZE 0    /*>0 to cache this number of bytes in lv_fs_read()*/
+    #define LV_FS_POSIX_LETTER 'L'      /*Drive letter used by LVGL: e.g. lv_img_set_src("L:/music.png")*/
+    #define LV_FS_POSIX_PATH "/littlefs"  /*LittleFS Arduino mount point on ESP32*/
+    #define LV_FS_POSIX_CACHE_SIZE 4096  /*Cache bytes for lv_fs_read(), speed up PNG decoding*/
 #endif
 
 /*API for CreateFile, ReadFile, etc*/
@@ -640,7 +640,7 @@
 #endif
 
 /*PNG decoder library*/
-#define LV_USE_PNG 0
+#define LV_USE_PNG 1
 
 /*BMP decoder library*/
 #define LV_USE_BMP 0
